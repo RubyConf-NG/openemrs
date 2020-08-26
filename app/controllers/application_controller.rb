009dc9c40fc7846b1ee936ebaf7826476a4de938
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-08-26 23:45:17 +0100

    refactor: rename some files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
 end
-
-class ApiController < ActionController::API
-  include V1::Response
-  include Error::ErrorHandler
-end
--- spec/helpers/homes_helper_spec.rb
+++ spec/helpers/homes_helper_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe StaticsHelper, type: :helper do
+RSpec.describe HomesHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
